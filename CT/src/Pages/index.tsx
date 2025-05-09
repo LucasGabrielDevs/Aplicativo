ce0aecc04aeb85c2a593fa7639702fe6917b8607
LucasGabrielDevs <lucasgabrieldev11@gmail.com>
2025-05-08 22:46:15 -0300

    incio de td

---
+++ CT/src/Pages/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import {StyleSheet, Text, View, Image, Button} from 'react-native'
+
+
+
---
+++ CT/src/Pages/styles.ts
@@ -0,0 +1,2 @@
+import React from "react";
+import {StyleSheet, View, Text, Button} from "react-native";
